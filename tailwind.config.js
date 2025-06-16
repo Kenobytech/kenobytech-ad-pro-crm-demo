d18b45bb70ac606c8e2cce03837f976eebdbe7b0
Kenobytech <Kenobytech@gmail.com>
2025-06-16 11:59:14 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      animation: {
-        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-        'bounce-slow': 'bounce 2s infinite',
-      },
-      backdropBlur: {
-        xs: '2px',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
